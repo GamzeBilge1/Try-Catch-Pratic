b72ef719da0e101ab187cf12b63821f515e22f95
Gamze <bilgegamze227@outlook.com>
2025-01-20 23:01:16 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+bool isValidInput = false; // isValidInput false olduğu sürece döngüye girilir
+
+while (!isValidInput)
+{
+    try
+    {
+        Console.WriteLine("Lütfen bir sayı giriniz");
+        int input = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("sayının karesi: " + input * input);
+
+        isValidInput = true; // Eğer dönüşüm başarılı olursa, isValidInput true yapılır
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Hatalı giriş yaptınız.Lütfen bir sayı giriniz"); // Eğer hata olursa, catch çalışır
+    }
+}
